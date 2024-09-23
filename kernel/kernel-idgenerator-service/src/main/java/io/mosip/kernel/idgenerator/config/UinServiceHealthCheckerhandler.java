f2a2ab462909d23db7283b312278138fde2a94f2
Nambi <nambirajan@mosip.io>
2024-09-23 17:45:55 +0530

    Id generator service restart issue analysis

--- kernel/kernel-idgenerator-service/src/main/java/io/mosip/kernel/idgenerator/config/UinServiceHealthCheckerhandler.java
+++ kernel/kernel-idgenerator-service/src/main/java/io/mosip/kernel/idgenerator/config/UinServiceHealthCheckerhandler.java
@@ -161,7 +161,9 @@ public class UinServiceHealthCheckerhandler implements HealthCheckHandler {
 			if (response.succeeded()) {
 				final JsonObject result = resultBuilder.create()
 						.add(UINHealthConstants.RESPONSE, response.result().body()).build();
-				future.complete(Status.OK(result));
+				if (!future.isComplete()) {
+					future.complete(Status.OK(result));
+				}
 			} else {
 				if (!future.isComplete()) {
 					future.complete(Status.KO());
